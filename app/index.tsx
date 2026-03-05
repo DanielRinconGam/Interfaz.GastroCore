138d05701870dc664d90dc9fb9c327e1dc137d47
Neyder-Dev <naquemba@ucompensar.edu.co>
2026-03-04 20:55:51 -0500

    Add: home, change: employees, main pages login, login go to home

--- app/index.tsx
+++ app/index.tsx
@@ -1,41 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
-import AppHeader from "../scr/components/layout/AppHeader";
-import SidebarMenu from "../scr/components/layout/SidebarMenu";
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return (
-    <View style={styles.page}>
-      {/* Header full width */}
-      <AppHeader restaurantName="Restaurante La 85" isOpen={true} />
-
-      {/* Body: Sidebar + Content */}
-      <View style={styles.body}>
-        <SidebarMenu />
-
-        <View style={styles.content}>
-          <Text style={styles.title}>Contenido de prueba</Text>
-        </View>
-      </View>
-    </View>
-  );
+  return <Redirect href="/tabs/login" />;
 }
-
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    flexDirection: "column",
-    backgroundColor: "#F8FAFC",
-  },
-  body: {
-    flex: 1,
-    flexDirection: "row",
-  },
-  content: {
-    flex: 1,
-    padding: 20,
-  },
-  title: {
-    fontSize: 22,
-    fontWeight: "bold",
-  },
-});
---
+++ app/tabs/home.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, View } from "react-native";
+import AppHeader from "../../scr/components/layout/AppHeader";
+import SidebarMenu from "../../scr/components/layout/SidebarMenu";
+
+export default function Index() {
+  return (
+    <View style={styles.page}>
+      {/* Header full width */}
+      <AppHeader restaurantName="Restaurante La 85" isOpen={true} />
+
+      {/* Body: Sidebar + Content */}
+      <View style={styles.body}>
+        <SidebarMenu />
+
+        <View style={styles.content}>
+         
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#F8FAFC",
+  },
+  body: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  content: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+});
